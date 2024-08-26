293369eb779942a66d540b51aea4185b2de220af
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2024-08-26 23:22:38 +0900

    admin-hotels-show

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ApiFetch } from "./components/ApiFetch";
+import { HotelShow } from "./components/admin/HotelShow";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ApiFetch />
+    <HotelShow />
   </React.StrictMode>
 );
 

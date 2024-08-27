7ededd1f5747f7b3918b556635c51c5970d8f4c5
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2024-08-28 00:53:10 +0900

    header-footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,13 @@
+import "./style.css";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <div className="d-flex justify-content-center align-items-center h-100">
+        <p className="text-center text-muted small mb-0">
+          &copy; Enjoyble Travel All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
